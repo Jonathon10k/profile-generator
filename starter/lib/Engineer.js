305613e9d91bd990587e245e573167068446f79b
Jonathon E <je20k3@gmail.com>
2024-02-06 13:57:30 +0000

    Add Manager component

--- starter/lib/Engineer.js
+++ starter/lib/Engineer.js
@@ -1,1 +1,29 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+
+const Employee = require("./Employee");
+
+class Manager extends Employee {
+    // Employee constructor
+    constructor(name, id, email, officeNumber) {
+        super(name, id, email);
+        this.officeNumber = officeNumber;
+    }
+
+    // Methods
+    getId() {
+        return this.id;
+    }
+
+    getEmail() {
+        return this.email;
+    }
+
+    getRole() {
+        return "Manager";
+    }
+}
+
+const Sarah = new Manager("Sarah", "2", "sarah@biz.com", 3);
+console.log(Sarah);
+
+module.exports = Manager;
