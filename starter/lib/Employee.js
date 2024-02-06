25b7569379879a3a8edbf00db01c7c335192d2a1
Jonathon E <je20k3@gmail.com>
2024-02-06 13:48:29 +0000

    Add gitignore

--- starter/lib/Employee.js
+++ starter/lib/Employee.js
@@ -1,1 +1,27 @@
 // TODO: Write code to define and export the Employee class
+
+class Employee {
+    // Employee constructor
+    constructor(name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+
+    // Methods
+    getId() {
+        return this.id;
+    }
+
+    getEmail() {
+        return this.email;
+    }
+
+    getRole() {
+        return "Employee";
+    }
+}
+
+let Joe = new Employee("Joe", "1", "joe@biz.com");
+console.log(Joe);
+module.exports = Employee;
